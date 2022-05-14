091dbf1f9487dc7a3aedba81ccf0cf11c7d2fb85
Arun <aruncrofficial@gmail.com>
2022-05-14 03:31:25 -0700

    normal list view

--- lib/screen_home.dart
+++ lib/screen_home.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
 
-class ScreenHome extends StatefulWidget {
+class ScreenHome extends StatelessWidget {
   const ScreenHome({Key? key}) : super(key: key);
 
   @override
-  State<ScreenHome> createState() => _ScreenHomeState();
-}
-
-class _ScreenHomeState extends State<ScreenHome> {
-  int _counter = 0;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          _counter = _counter + 1;
-          setState(() {
-            _counter;
-          });
-        },
-      ),
-      // body: SafeArea(child: Center(child: Text(_counter.toString()))),
-      body: SafeArea(child: Center(child:DisplayText(counterText: _counter)
+      body: SafeArea(
+          child: ListView(
+        children: List.generate(100, (index) {
+          return Column(
+            children: [Text(index.toString()), const Divider()],
+          );
+        }),
       )),
     );
-  }
-}
-
-class DisplayText extends StatelessWidget {
-  final int counterText;
-
-  const DisplayText({Key? key, required this.counterText}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(counterText.toString());
   }
 }
