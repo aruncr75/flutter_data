88612a093f1d193e7dad0d7ede2de80bc88f9214
Arun <aruncrofficial@gmail.com>
2022-06-15 10:26:46 -0700

    time difference -office-work

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -6,23 +6,46 @@ class ScreenHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // var date = new DateTime(2018, 1, 13);
+    var _currentDate = DateTime.now();
+    var date1 = DateTime.parse("2020-05-21T05:26:18.651Z");
+    var date2 = DateTime.parse("2020-05-21T05:26:18.651Z");
+    final difference = date2.difference(date1).inDays;
+    print(difference);
     return DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
           body: Column(children: [
-            TabBar(tabs: [Tab(
-            child: Text("Home",style: TextStyle(color: Colors.black))),
-            Tab(child: Text("Serach",style: TextStyle(color: Colors.black),),),
-            Tab(child: Text("Accounts",style: TextStyle(color: Colors.black),),)
-            ]
-            ),Expanded(
-              child: TabBarView(children: [Container(child: Center(child: Text("Home"))),
-              Container(child: Center(child: Text("Search"))),
-              Container(child: Center(child: Text("Account")))]),
+            const TabBar(tabs: [
+              Tab(child: Text("Home", style: TextStyle(color: Colors.black))),
+              Tab(
+                child: Text(
+                  "Serach",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Tab(
+                child: Text(  
+                  "Accounts",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  "Time Difference",
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ]),
+            Expanded(
+              child: TabBarView(children: [
+                Container(child: const Center(child: Text("Home"))),
+                Container(child: const Center(child: Text("Search"))),
+                Container(child: Center(child: Text("Account"))),
+                Container(child: Center(child: Text("${difference}",style: TextStyle(color: difference >3 ? Colors.red:Colors.blue),)))
+              ]),
             )
-            ]
-            ),
+          ]),
         ));
   }
 }
-
