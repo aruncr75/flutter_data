a7f4cbabb251cb6b5036504deea201a9c1c31760
aruncr75 <aruncr75@gmail.com>
2023-01-31 20:36:37 +0530

    HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firstapp1/screens/firebase_notification.dart';
+import 'package:firstapp1/screens/ScreenHome/screen_home.dart';
 import 'package:flutter/material.dart';
 // adb connect 192.168.43.1:5555
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   runApp(const MyApp());
 }
 
@@ -17,13 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ScreenNotification(),
+      home: ScreenHome(),
     );
   }
 }
 
-Future<void> backgroundHandler(RemoteMessage message) async {
-  print("this Message is from Backgroung");
-  print("${message.notification!.title}");
-  print("${message.notification!.body}");
-}
+
--- lib/screens/ScreenSplash/splash.dart
+++ lib/screens/ScreenSplash/splash.dart
@@ -1,5 +1,5 @@
 
-import 'package:firstapp1/screens/login.dart';
+import 'package:firstapp1/screens/ScreenHome/screen_home.dart';
 import 'package:flutter/material.dart';
 
 class ScreenSplash extends StatefulWidget {
@@ -45,6 +45,6 @@ class _ScreenSplashState extends State<ScreenSplash> {
   Future<void> goToLogin() async {
     await Future.delayed(const Duration(milliseconds: 100));
     Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) =>  ScreenLogin()));
+        .push(MaterialPageRoute(builder: (context) =>  const ScreenHome()));
   }
 }
